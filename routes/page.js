7750b6adafefb5184f9958f7e17b9b3b771d26e8
audwo <kmjstj3@naver.com>
2023-09-11 14:31:55 +0900

    pageRoute

---
+++ routes/page.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const {
+  renderJoin,
+  renderMain,
+  renderProfile,
+} = require("../controllers/page");
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+
+router.use((req, res, next) => {
+  //res.locals미들에워 간에 공유되는 데이터
+  res.locals.user = req.user;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followingIdList = [];
+  //res.sessiom:사용자의 고유한 데이터
+  next();
+});
+
+router.get("/profile", isLoggedIn, renderProfile);
+router.get("/join", isNotLoggedIn, renderJoin);
+
+router.get("/", renderMain);
+module.exports = router;
